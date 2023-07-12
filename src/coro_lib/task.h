ced7e1a40df4a6743d423634e915a440cbb62181
Mircea Baja <bajamircea@yahoo.com>
2023-07-12 22:36:12 +0100

    Use task_promise_result

--- src/coro_lib/task.h
+++ src/coro_lib/task.h
@@ -22,54 +22,58 @@ namespace coro
   template<typename Promise>
   using scoped_coroutine_handle = cpp_util::unique_handle<scoped_coroutine_handle_traits<Promise>>;
 
-  template<typename Promise>
-  struct task_promise_final_await
+  template<typename T>
+  class task_promise;
+
+  template<typename T>
+  class [[nodiscard]] task
   {
-    [[nodiscard]] constexpr bool await_ready() const noexcept
+  public:
+    using result_type = T;
+    using promise_type = task_promise<T>;
+
+  private:
+    scoped_coroutine_handle<promise_type> task_coro_;
+
+    friend struct task_promise<T>;
+
+    task(std::coroutine_handle<promise_type> task_coro) noexcept : task_coro_(task_coro)
     {
-      return false;
     }
 
-    auto await_suspend(std::coroutine_handle<Promise> promise_coro) noexcept
+    struct awaiter
     {
-      return promise_coro.promise().final_await_suspend();
-    }
+      std::coroutine_handle<promise_type> task_coro;
 
-    [[noreturn]] constexpr void await_resume() const noexcept
+      [[nodiscard]] constexpr bool await_ready() const noexcept
+      {
+        return false;
+      }
+
+      std::coroutine_handle<> await_suspend(std::coroutine_handle<> continuation_coro) noexcept
+      {
+        assert(!task_coro.promise().continuation_coro_);
+        task_coro.promise().continuation_coro_ = continuation_coro;
+        return task_coro;
+      }
+
+      T await_resume()
+      {
+        return task_coro.promise().get_result();
+      }
+    };
+
+  public:
+    awaiter operator co_await() &&
     {
-      std::terminate();
+      return awaiter{task_coro_.get()};
     }
   };
 
   template<typename T>
-  class task;
-
-  template<typename T>
-  struct task_promise
+  struct task_promise_result
   {
-    std::coroutine_handle<> continuation_coro_;
     std::variant<std::monostate, T, std::exception_ptr> result_;
-
-    task_promise() noexcept : continuation_coro_{}, result_{}
-    {
-    }
-
-    task<T> get_return_object() noexcept;
-
-    std::suspend_always initial_suspend() noexcept
-    {
-      return {};
-    }
-
-    task_promise_final_await<task_promise> final_suspend() noexcept
-    {
-      return {};
-    }
-
-    std::coroutine_handle<> final_await_suspend() noexcept
-    {
-      return continuation_coro_;
-    }
 
     template<typename U>
       requires std::convertible_to<U, T>
@@ -98,31 +102,9 @@ namespace coro
   };
 
   template<>
-  struct task_promise<void>
+  struct task_promise_result<void>
   {
-    std::coroutine_handle<> continuation_coro_;
     std::exception_ptr exception_;
-
-    task_promise() noexcept : continuation_coro_{}, exception_{}
-    {
-    }
-
-    task<void> get_return_object() noexcept;
-
-    std::suspend_always initial_suspend() noexcept
-    {
-      return {};
-    }
-
-    task_promise_final_await<task_promise> final_suspend() noexcept
-    {
-      return {};
-    }
-
-    std::coroutine_handle<> final_await_suspend() noexcept
-    {
-      return continuation_coro_;
-    }
 
     void return_void() noexcept
     {
@@ -142,59 +124,43 @@ namespace coro
     }
   };
 
-  template<typename Task>
-  struct task_awaiter
+  template<typename T>
+  struct task_promise : public task_promise_result<T>
   {
-    std::coroutine_handle<typename Task::promise_type> task_coro;
+    std::coroutine_handle<> continuation_coro_;
 
-    [[nodiscard]] constexpr bool await_ready() const noexcept
+    task<T> get_return_object() noexcept
     {
-      return false;
+        return task<T>{std::coroutine_handle<task_promise<T>>::from_promise(*this)};
     }
 
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<> continuation_coro) noexcept
+
+    std::suspend_always initial_suspend() noexcept
     {
-      assert(!task_coro.promise().continuation_coro_);
-      task_coro.promise().continuation_coro_ = continuation_coro;
-      return task_coro;
+      return {};
     }
 
-    auto await_resume()
+    struct final_await
     {
-      return task_coro.promise().get_result();
+      [[nodiscard]] constexpr bool await_ready() const noexcept
+      {
+        return false;
+      }
+
+      auto await_suspend(std::coroutine_handle<task_promise> promise_coro) noexcept
+      {
+        return promise_coro.promise().continuation_coro_;
+      }
+
+      [[noreturn]] constexpr void await_resume() const noexcept
+      {
+        std::terminate();
+      }
+    };
+
+    final_await final_suspend() noexcept
+    {
+      return {};
     }
   };
-
-  template<typename T>
-  class [[nodiscard]] task
-  {
-  public:
-    using result_type = T;
-    using promise_type = task_promise<T>;
-
-  private:
-    scoped_coroutine_handle<promise_type> task_coro_;
-
-    friend struct task_promise<T>;
-
-    task(std::coroutine_handle<promise_type> task_coro) noexcept : task_coro_(task_coro)
-    {
-    }
-  public:
-    auto operator co_await() &&
-    {
-      return task_awaiter<task>{task_coro_.get()};
-    }
-  };
-
-  template<typename T>
-  task<T> task_promise<T>::get_return_object() noexcept
-  {
-      return task<T>{std::coroutine_handle<task_promise<T>>::from_promise(*this)};
-  }
-
-  task<void> task_promise<void>::get_return_object() noexcept
-  {
-      return task<void>{std::coroutine_handle<task_promise<void>>::from_promise(*this)};
-  }
 }
