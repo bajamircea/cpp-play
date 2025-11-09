6d1d5bd5e420f39117758ea0a6b8563022093605
Mircea Baja <bajamircea@yahoo.com>
2025-11-09 09:40:37 +0000

    Large but single file reproduces

--- src/coro_st_lib/coro_st.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "callback.h"
-#include "stop_util.h"
-#include "context.h"
-#include "coro_type_traits.h"
-#include "co.h"
-#include "suspend_forever.h"
-#include "stop_when.h"
