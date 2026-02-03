bc0d484e6e715115b6ab91350bc81f1b5dc697b7
Mircea Baja <bajamircea@yahoo.com>
2026-02-03 21:20:31 +0000

    Event examples

--- src/coro_st_lib_test/event_test.cpp
+++ src/coro_st_lib_test/event_test.cpp
@@ -5,6 +5,7 @@
 #include "../coro_st_lib/co.h"
 #include "../coro_st_lib/coro_type_traits.h"
 #include "../coro_st_lib/run.h"
+#include "../coro_st_lib/wait_all.h"
 
 #include "test_loop.h"
 
@@ -134,6 +135,66 @@ namespace
     tl.run_one_ready();
     ASSERT_FALSE(tl.result_ready);
     ASSERT_TRUE(tl.stopped);
+  }
+
+  coro_st::co<void> async_event_notifier1(coro_st::event& evt)
+  {
+      evt.notify_all();
+      co_return;
+  }
+
+  coro_st::co<void> async_event_waiter1(coro_st::event& evt)
+  {
+      co_await evt.async_wait();
+  }
+
+  TEST(event_usage1)
+  {
+      coro_st::event evt;
+      auto result = coro_st::run(
+          coro_st::async_wait_all(
+              async_event_waiter1(evt),
+              async_event_notifier1(evt)));
+      ASSERT_TRUE(result.has_value());
+  }
+
+  struct event_usage_data2
+  {
+      int counter{ 0 };
+      bool event_is_set{ false };
+      coro_st::event evt;
+  };
+
+  coro_st::co<void> async_event_notifier2(event_usage_data2& data)
+  {
+      data.event_is_set = true;
+      data.evt.notify_all();
+      co_return;
+  }
+
+  coro_st::co<bool> async_event_waiter2(event_usage_data2& data)
+  {
+      if (data.event_is_set)
+      {
+          data.counter += 40;
+          co_return false;
+      }
+      co_await data.evt.async_wait();
+      data.counter += 2;
+      co_return true;
+  }
+
+  TEST(event_usage2)
+  {
+      event_usage_data2 data;
+      auto result = coro_st::run(
+          coro_st::async_wait_all(
+              async_event_waiter2(data),
+              async_event_notifier2(data),
+              async_event_waiter2(data))).value();
+      ASSERT_TRUE(std::get<0>(result));
+      ASSERT_FALSE(std::get<2>(result));
+      ASSERT_EQ(42, data.counter);
   }
 
   // coro_st::co<void> async_event_does_not_compile(coro_st::event& evt)
